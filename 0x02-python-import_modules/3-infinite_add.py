5d4c9faf061870cd3f4386596f7d22ee92646215
AUSTINMORARA <austiinmorara@gmail.com>
2023-01-17 21:38:39 -0800

    addition on command line

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,13 @@
+#!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
 
-length = len(argv)
-
+argc = len(argv)
 result = 0
-
-if length == 1:
+if argc == 1:
     print(f"{result:d}")
 else:
-    for i in range(1, length):
+    for i in range(1, argc):
         result += int(argv[i])
     print(f"{result:d}")
     
