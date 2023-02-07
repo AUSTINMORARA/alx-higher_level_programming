0806a9c8d013305653dc9440a91ffbf15b0d0c59
AUSTINMORARA <austiinmorara@gmail.com>
2023-02-07 07:31:53 -0800

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+defination of class
+returns list
+'''
+
+
+class MyList:
+	'''
+	class declaration
+	'''
+	def lookup(obj):
+	'''
+	function that returns the list
+		Args:
+			obj:object to be listed
+	'''
+		return dir(obj)
