4eb6e23260952fa8079a8433f77388681a896df3
AUSTINMORARA <austiinmorara@gmail.com>
2023-02-12 02:52:56 -0800

    indentation in 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
     '''replaces all occurrences of an element'''
-   return [replace if i is search else i for i in my_list]
+    return [replace if i is search else i for i in my_list]
