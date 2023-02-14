61d03d1a4fa7b2858a3dc3d4d9f5b37762075d24
AUSTINMORARA <austiinmorara@gmail.com>
2023-02-14 01:00:35 -0800

    unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-    '''adds all unique integers in a list'''
-    s = 0
-    for i in my_list:
-       set (s += i)
-    return s
+def uniq_add(my_list=0):
+	'''adds all unique integers in a list'''
+	it_set = set(my_list)
+
+	s = 0
+	for i in it_set:
+		s += i
+	return s
