9518d40c1fd0ab4172a94e5ddf11fa4e8ed8eb8d
AUSTINMORARA <austiinmorara@gmail.com>
2023-03-09 12:56:23 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,18 +2,18 @@
 
 if __name__ == '__main__':
     from sys import argv
-    
-    l = len(argv)
+
+    length = len(argv)
 
     index = 1
 
-    if l == 1:
+    if lenght == 1:
         print("0 arguments.")
-    elif l == 2:
+    elif length == 2:
         print("1 argument:")
         print(f"1: {argv[index]}")
     else:
         print(f"{len(argv)} arguments:")
-        while index != l:
+        while index != length:
             print(f"{index}: {argv[index]}")
             index += 1
