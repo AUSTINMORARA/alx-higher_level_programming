02833053f4dc39c89f97d865ad48041dfa3fa25e
AUSTINMORARA <austiinmorara@gmail.com>
2023-03-17 00:37:18 -0700

    replacing

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     '''replaces all occurrences of an element'''
-    return [replace if i is search else i for i in my_list]
+    new = []
+    for i in my_list:
+        if i == search:
+            i = replace
+            new.append(i)
+        else:
+            new.append(i)
+    return new
