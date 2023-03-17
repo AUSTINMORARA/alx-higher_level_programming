cb24a4d5dc7ac3df6d01847b1362917c31fcaecd
AUSTINMORARA <austiinmorara@gmail.com>
2023-03-17 01:10:47 -0700

    unique element

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=0):
     '''adds all unique integers in a list'''
-    it_set = set(my_list)
-
-    s = 0
-    for i in it_set:
-    	s += i
-    return s
+    set_it = set(my_list)
+   
+    sm = 0
+    for i in set_it:
+        sm += i
+    return sm
