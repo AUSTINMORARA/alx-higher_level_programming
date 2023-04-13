d2553780677a1b81e94b46b333adf5cde862cd03
AUSTINMORARA <austiinmorara@gmail.com>
2023-04-13 16:19:04 -0700

    using dir

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-'''
-defination of class
-returns list
-'''
+""" Lookup module """
 
 
-class MyList:
-	'''
-	class declaration
-	'''
-	def lookup(obj):
-	'''
-	function that returns the list
-		Args:
-			obj:object to be listed
-	'''
-		return dir(obj)
+def lookup(obj):
+    '''
+    function that returns the list
+    Args:
+	obj:object to be listed
+    '''
+    return dir(obj)
