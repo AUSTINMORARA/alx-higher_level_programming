1e518cf0742e25ab46c6aedd68b5edc45061bf98
AUSTINMORARA <austiinmorara@gmail.com>
2023-05-31 10:41:58 -0700

    #squares

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+Square class defination
+'''
+
+
+class Square:
+    '''Square class declaration'''
+    def __init__(self, size=0):
+        '''Class initialization
+            Args:
+                size:size of square
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''Retrieves size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''size setter
+            Args:
+                value: to be evaluated
+        '''
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        '''Calculates the area of a square.'''
+        return self.__size * self.__size
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end = "")
+            print('\n')
