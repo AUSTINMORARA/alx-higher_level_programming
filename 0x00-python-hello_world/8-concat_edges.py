0eb9d3a3c785e3d53c46804801802b9d008d4aa9
AUSTINMORARA <austiinmorara@gmail.com>
2023-11-27 23:33:10 +0300

    $commitmessage

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + " " + str[107:111] + " " + str[:6]
+space = " "
+str = str[39:66] + space + str[107:111] + space + str[:6]
 print(str)
