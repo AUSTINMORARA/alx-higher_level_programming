da5e1a497a56a9497a7b0d220f20f59c446f9f5c
AUSTINMORARA <austiinmorara@gmail.com>
2023-11-27 23:34:57 +0300

    $commitmessage

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 str1 = "Holberton"
 str2 = "School"
-space = " "
+space = ' '
 str1 = str1 + space + str2
 print(f"Welcome to {str1}!")
--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-space = " "
+space = ' '
 str = str[39:66] + space + str[107:111] + space + str[:6]
 print(str)
