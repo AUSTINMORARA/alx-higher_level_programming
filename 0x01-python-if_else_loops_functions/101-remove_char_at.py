2a79dacbd27ea8290190d090a698bbae06cb338f
Austin Morara <111016289+AUSTINMORARA@users.noreply.github.com>
2024-02-22 13:49:28 +0300

    Add files via upload

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+def remove_char_at(str, n):
+    str1 = str
+    output = ''
+    index = 0
+    for char in str1:
+        if index == n:
+            index += 1
+            continue
+        else:
+            output += char
+            index += 1
+    return output
